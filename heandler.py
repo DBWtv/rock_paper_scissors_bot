5ee1bebb1f60e1988e8831699b86b650a34101aa
DBWtv <dima.bwc@gmail.com>
2023-01-19 16:25:04 +0600

    voice message heandler

--- heandler.py
+++ heandler.py
@@ -23,9 +23,23 @@ async def process_hi_comand(message: types.Message):
 async def send_echo(message: types.Message):
     await message.reply(message.text)
 
+
+async def send_photo_echo(message: types.Message):
+    await message.answer_photo(message.photo[0].file_id)
+
+async def send_sticker_echo(message: types.Message):
+    await message.answer_sticker(message.sticker.file_id)
+
+async def proccess_audio_message(message: types.Message):
+    print(message.voice)
+    await message.answer(text='Я не умею обрабатывать голосовые сообщения')
+
 dp.register_message_handler(process_start_command, commands='start')
 dp.register_message_handler(process_help_comand, commands='help')
 dp.register_message_handler(process_hi_comand, commands='say_hi')
+dp.register_message_handler(proccess_audio_message, content_types=['voice'])
+dp.register_message_handler(send_photo_echo, content_types=['photo'])
+dp.register_message_handler(send_sticker_echo, content_types=['sticker'])
 dp.register_message_handler(send_echo)
 
 
