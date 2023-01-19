aadaafda21681f45857004dd11d3eaf2a7c150d3
DBWtv <dima.bwc@gmail.com>
2023-01-19 14:45:18 +0600

    long polling

--- heandler.py
+++ heandler.py
@@ -4,30 +4,23 @@ from acces_token import token
 
 API_URL = 'https://api.telegram.org/bot'
 BOT_TOKEN = token
-TEXT: str = 'Hello there General Kenobi!'
-MAX_COUNTER: int = 100
-
 offset: int = -2
-counter: int = 0
-chat_id: int
+timeout: int = 60
+updates: dict
 
-while counter < MAX_COUNTER:
-    print(f'attempt = {counter}')
-    updates = requests.get(
-        f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset+1}').json()
+
+def do_something() -> None:
+    print('Был апдейт')
+
+
+while True: 
+    start_time = time.time()
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}&timeout={timeout}').json()
 
     if updates['result']:
         for result in updates['result']:
             offset = result['update_id']
-            chat_id = result['message']['from']['id']
-            photo_url = requests.get('https://aws.random.cat/meow')
-            if photo_url.status_code == 200:
-                cat_photo_url = photo_url.json()['file']
-                requests.get(
-                    f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_photo_url}')
-            else:
-                requests.get(
-                    f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text=Не получилсоь, не фартануло')
+            do_something()
 
-            time.sleep(1)
-            counter += 1
+    end_time = time.time()
+    print(f'Время между запросами к Telegram Bot API: {end_time - start_time}')
